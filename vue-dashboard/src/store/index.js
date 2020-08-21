cd5088ab69209e38279ab68b8a0c72aa45002e4e
luanyoushu <touch@touchdeMacBook-Pro.local>
2020-08-21 18:06:54 +0800

    vuex 开始使用

--- vue-dashboard/src/store/index.js
+++ vue-dashboard/src/store/index.js
@@ -5,11 +5,25 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 1
   },
   mutations: {
+    increment (state, payload) {
+      // 变更状态
+      state.count++
+    }
   },
   actions: {
+    increment (context, payload) {
+      context.commit('increment', payload)
+    }
   },
   modules: {
   }
 })
+// store.commit('increment', 10)
+// store.commit('increment', {
+//   amount: 10
+// })
+
+// store.dispatch('increment') // action
--- vue-dashboard/src/views/Home.vue
+++ vue-dashboard/src/views/Home.vue
@@ -2,13 +2,29 @@
   <div class="home">
     <img alt="Vue logo"
          src="../assets/logo.png">
-         this is my home
+         this is my home {{ getCount }}
+
+         <div @click="testAction"> aaaa </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  computed: {
+    getCount () {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    testAction () {
+      console.log('[testAction]')
+      // this.$store.commit('increment', {
+      //   amount: 100
+      // })
+      this.$store.dispatch('increment', { amount: 101 })
+    }
+  },
   mounted () {
     console.log(this.$route)
   }
